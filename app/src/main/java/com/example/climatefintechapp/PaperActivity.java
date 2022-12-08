a815801027a49e745546be38f680f74a88f13c17
mukundsrepository <60322472+mukundsrepository@users.noreply.github.com>
2022-12-08 13:47:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/climatefintechapp/PaperActivity.java
@@ -0,0 +1,14 @@
+package com.example.climatefintechapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paper);
+    }
+}
